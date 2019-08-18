5ad5ba024747169751b1ec59319db674561dbf0a
hwasub Lee <peaceflame9@gmail.com>
2019-08-18 19:37:58 +0900

    #2.25 More Controllers Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,29 @@
 //res.render( 탬플릿 , 탬플릿에 추가할 정보가 담길 객체 )
+import { videos } from "../db";
+import routes from "../routes";
 
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  //const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
 
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
-
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  //To DO : Upload and Save Video
+  res.redirect(routes.videoDetail(3572921));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 3572921,
+    title: "Video Awesome",
+    description: "This is Test",
+    views: 24,
+    videoFile: "http://v2v.cc/~j/theora_testsuite/320x240.ogg",
+    creator: {
+      id: 121212,
+      name: "Hwas",
+      email: "hwas@seb.com"
+    }
+  },
+  {
+    id: 3572922,
+    title: "Video Awesome2",
+    description: "This is Test",
+    views: 24,
+    videoFile: "http://v2v.cc/~j/theora_testsuite/320x240.ogg",
+    creator: {
+      id: 121212,
+      name: "Hwas",
+      email: "hwas@seb.com"
+    }
+  },
+  {
+    id: 3572923,
+    title: "Video Awesome3",
+    description: "This is Test",
+    views: 24,
+    videoFile: "http://v2v.cc/~j/theora_testsuite/320x240.ogg",
+    creator: {
+      id: 121212,
+      name: "Hwas",
+      email: "hwas@seb.com"
+    }
+  },
+  {
+    id: 3572924,
+    title: "Video Awesome4",
+    description: "This is Test",
+    views: 24,
+    videoFile: "http://v2v.cc/~j/theora_testsuite/320x240.ogg",
+    creator: {
+      id: 121212,
+      name: "Hwas",
+      email: "hwas@seb.com"
+    }
+  }
+];
